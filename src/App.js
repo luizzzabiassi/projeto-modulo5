d09b5ccfd7a5ca34fdff70b950a840b298eea50d
Camisao.Lucas <lcscms326@gmail.com>
2021-08-24 00:33:27 -0300

    Criando Home e rotas para demais páginas
    Além da Home foram organizadas as pastas para melhor visualização

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import './Assets/App.css';
+import './Assets/index.css'
+
+
+import Header from './Components/Header/Header';
+import Content from './Components/Content/Content';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Content>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+    </Switch>
+    </Content>
+    <Footer />
+  </BrowserRouter>
   );
 }
 
