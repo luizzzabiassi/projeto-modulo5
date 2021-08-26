67b6f07991f168d9540f2419cda1cb1e659094a6
victorsilvajc021 <luanyss@outlook.com>
2021-08-25 22:22:54 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Drinks from './Components/Drinks/Drinks';
+]]]]]]]]]]]]]]]]]]]]
 
 function App() {
   return (
