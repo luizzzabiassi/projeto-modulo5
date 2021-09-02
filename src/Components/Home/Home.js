a8957bac38b6bd7d44363b961049ebb4305fba47
Luiza Biassi <luiza.biassi@gmail.com>
2021-09-02 18:19:08 -0300

    home update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,8 @@ import style from "./Home.module.css";
 const Home = () => {
   return (
     <main className={style.main}>
-        
+      <h1 className={style.h1}>Seja bem-vindo ao Máfia's PUB!</h1>
+      <h3 className={style.h1}>Aqui, a sua diversão é garantida!</h3>
     </main>
   );
 };
