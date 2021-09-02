940c85189d791713f57eeb51c9de7dbc4ebd7017
Luiza Biassi <luiza.biassi@gmail.com>
2021-09-02 00:24:48 -0300

    estilizando drinks page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './Assets/App.css';
 import './Assets/index.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
